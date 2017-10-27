6fce97446d85c559403f9deb22fefceb57fbd07e
alisson_ramos <alisson_ramos@SNRSL1ELBIET040>
2017-10-27 11:04:43 -0200

    criei novas paginas para o menu

---
+++ home.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<!--grid layout - layout ajustavel-->
+<html>
+	<head>
+		<title>grid layout</title>
+		<meta charset="utf-8">
+                <link type="text/css" href="css/estilo.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="container">
+			<!--cabecalho da pagina-->
+			<header class="col-12">
+                            <img src="img/logo.png" class="logo">
+			</header>
+			<!--./cabecalho da pagina-->
+			<!--menu-->
+			<nav class="col-12">
+				<ul class="menu">
+					<li>
+						<a href="home.php">
+							Home
+						</a>
+					</li>
+					<li>
+                                                <a href="nossaempresa.php">
+							Nossa Empresa
+						</a>
+					</li>
+					<li>
+						<a href="produtos.php">
+							Produtos
+						</a>
+					</li>
+					<li>
+						<a href="nossaequipe.php">
+							Nossa Equipe
+						</a>
+					</li>
+				</ul>
+			</nav>
+                        <!--./menu-->
+			<!--banner-->
+			<article class="col-12">
+				<img src="fotos/banner.png" class="banner" alt="banner">
+			</article>
+			<!--./banner-->
+			<!--conteudo-->
+			<section class="col-12">
+				<h3>mais vendidas</h3>
+				<div class="col-4">
+					<img src="fotos/foto1.jpg" class="fotos" alt="foto 01">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto2.jpg" class="fotos" alt="foto 02">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto3.jpg" class="fotos" alt="foto 03">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto4.jpg" class="fotos" alt="foto 04">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto5.jpg" class="fotos" alt="foto 04">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto6.jpg" class="fotos" alt="foto 04">
+				</div>
+                                <div class="col-4">
+					<img src="fotos/foto7.jpg" class="fotos" alt="foto 04">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto8.jpg" class="fotos" alt="foto 04">
+				</div>
+			</section>
+			<!--conteudo-->
+			<section id="conteudo">
+				<div class="titulo"> 
+				</div>
+				<div class="dados">
+			<!--rodape-->
+			<footer class="col-12 rodape">
+				<p>Todos os direitos reservados - &copy;2017.</p>
+			</footer>
+			<!--./rodape-->
+		</div>
+	</body>
+</html>
