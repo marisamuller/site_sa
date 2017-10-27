e3fe0a45985b2f0f9767c9732590eb4312cbd69b
marisa_muller <marisa_muller@SNRSL1ELBIET045>
2017-10-27 10:05:41 -0200

    index estilo

--- index.php
+++ index.php
@@ -1,17 +1,87 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<!--grid layout - layout ajustavel-->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+	<head>
+		<title>grid layout</title>
+		<meta charset="utf-8">
+		<link type="text/css" href="estilo.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="container">
+			<!--cabecalho da pagina-->
+			<header class="col-12">
+				<img src="fotos/logo.jpg" class="logo">
+			</header>
+			<!--./cabecalho da pagina-->
+			<!--menu-->
+			<nav class="col-12">
+				<ul class="menu">
+					<li>
+						<a href="home.html">
+							Home
+						</a>
+					</li>
+					<li>
+						<a href="nossaempresa.html">
+							Nossa Empresa
+						</a>
+					</li>
+					<li>
+						<a href="tatuagens.html">
+							Tatuagens
+						</a>
+					</li>
+					<li>
+						<a href="depoimentos.html">
+							Depoimentos
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<!--./menu-->
+			<!--banner-->
+			<article class="col-12">
+				<img src="fotos/banner.png" class="banner" alt="banner">
+			</article>
+			<!--./banner-->
+			<!--conteudo-->
+			<section class="col-12">
+				<h3>mais vendidas</h3>
+				<div class="col-4">
+					<img src="fotos/foto1.jpg" class="fotos" alt="foto 01">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto2.jpg" class="fotos" alt="foto 02">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto3.jpg" class="fotos" alt="foto 03">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto4.jpg" class="fotos" alt="foto 04">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto5.jpg" class="fotos" alt="foto 04">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto6.jpg" class="fotos" alt="foto 04">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto7.jpg" class="fotos" alt="foto 04">
+				</div>
+				<div class="col-4">
+					<img src="fotos/foto8.jpg" class="fotos" alt="foto 04">
+				</div>
+			</section>
+			<!--conteudo-->
+			<section id="conteudo">
+				<div class="titulo"> 
+				</div>
+				<div class="dados">
+			<!--rodape-->
+			<footer class="col-12 rodape">
+				<p>Todos os direitos reservados - &copy;2017.</p>
+			</footer>
+			<!--./rodape-->
+		</div>
+	</body>
 </html>
