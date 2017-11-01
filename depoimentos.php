1cc3b811cb11e8a3035bea5d2dcb7b97af2caa35
alisson_ramos <alisson_ramos@SNRSL1ELBIET040>
2017-11-01 10:51:06 -0200

    DEPOIMENTOS

--- depoimentos.php
+++ depoimentos.php
@@ -46,17 +46,17 @@
             <!--./banner-->
             <!--conteudo-->
             <section class="col-12">
-                <h3>depoimentos do que é tatuagen para você</h3>
+                <h3>depoimentos do que é a empresa T.I.Informática para você</h3>
                 <div>
-                    <h3></h3>
+                    <h3>Equipe nota 10, tanto no desenvolvimento do site como na manutenção do mesmo , apesar da equipe aparentar ser formada por jovens todos são comprometidos com o trabalho.</h3>
                     <hr>
-                    <h3></h3>
+                    <h3>A equipe da T.I.Informática nos surpreendeu com tamanha competência, agilidade e qualidade nos seus trabalhos, sempre a disposição quando precisamos. Parabenizo esse time por dar uma cara nova a nossa empresa, nos apresentando um site incrível, objetivo e super completo. Recomendo a T.I.Informática, e desejo sucesso profissional a todos!</h3>
                     <hr>
-                    <h3></h3>
+                    <h3>Agradeço a atenção e dedicação de toda equipe, fui muito bem assessorada, as solicitações sempre atendidas, estou satisfeita! Com certeza irei recomendar. Que nossa parceria continue assim.</h3>
                     <hr>
-                    <h3></h3>
+                    <h3>Gostaria de agradecer a toda a equipe da T.I.Informática, fizerem um trabalho brilhante no desenvolvimento de nosso site, com profissionalismo, agilidade e toda técnica apurada e total transparência. Realmente um investimento recompensador !!</h3>
                     <hr>
-                    <h3></h3>
+                    <h3>A T.I.Informática sempre supera as minhas expectativas quanto ao atendimento e serviços prestados. É para mim uma empresa de referência no mercado que priva sempre pela qualidade e o mais importante que acho: que conseguiu o comprometimento de sua equipe para o mesmo fim, o que faz uma enorme diferença, no que se refere à confiança e pronto atendimento pelos serviços prestados. Consequentemente isso a torna uma empresa que me dá a plena satisfação e a vontade se sempre ampliar a relação comercial que temos. Parabéns a toda a equipe!</h3>
 		</div>
             </section>
             <!--rodape-->
